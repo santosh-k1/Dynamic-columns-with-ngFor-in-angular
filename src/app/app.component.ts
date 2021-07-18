ec9deb3af7c75c715c2eda2e27b24914bfb1d4bf
santosh-k1 <ui.santoshkumar@gmail.com>
2021-07-19 00:17:41 +0530

    Dynamic Columns with angular loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+  constructor() {}
+  dataList = [
+    { Name: 'Zhang San', gender: 'Men', age: '10' },
+    { Name: 'Li Si', gender: 'female', age: '20' },
+    { Name: 'Wang Wu', gender: 'Men', Age: '30' }
+  ];
+  ngOnInit(): void {
+    console.log(this.dataList);
+  }
   name = 'Angular ' + VERSION.major;
+
+  getKeys(item) {
+    return Object.keys(item);
+  }
 }
